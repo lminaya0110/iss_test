08323e6c3f1e784409a294b5f6389ca4bbba73fc
lminaya0110 <100318087+lminaya0110@users.noreply.github.com>
2023-09-19 12:29:43 -0400

    successfully writing people in space, lat/long

---
+++ main.py
@@ -0,0 +1,23 @@
+import json
+import turtle
+import urllib.request
+import time
+import webbrowser
+import geocoder
+
+url = "http://api.open-notify.org/astros.json"
+response = urllib.request.urlopen(url)
+result = json.loads(response.read())
+
+file = open("iss.txt", "w")
+file.write("There are currently" + str(result['number']) + "astronauts on the ISS: \n\n")
+
+people = result['people']
+for person in people:
+    file.write(person['name'] + " - on board " + "\n")
+
+# print Long and Lat
+g = geocoder.ip('me')
+file.write("\n Your current Lat / Long is : " + str(g.latlng))
+file.close()
+webbrowser.open("iss.txt")
